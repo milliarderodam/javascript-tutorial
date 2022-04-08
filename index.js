6b7c238ab4885e94fb4047f2a1a227cbca8b7122
Milliarderodam <milliarderodam@gmail.com>
2022-04-08 10:54:14 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+"use strict";
+// let name = {
+//   Ism: "Baxtishod",
+//   Familiya: "Zoyirov",
+//   Yosh: 19,
+//   Ish: "Dasturchi",
+//   isMarried: false,
+// };
+
+// console.log(name.Familiya);
+
+// let hello = ["You", "I am", "This"];
+// console.log(hello[3]);
+
+// alert("Hello world");
+
+// const name = confirm("Hello");
+// console.log(name);
+
+// let name = prompt("Siz qayerda yashaysiz");
+// console.log(name);
+
+// const menu = [];
+// menu[0] = prompt("ISmiz nima?");
+// menu[1] = prompt("Yoshiz nechida?");
+// menu[2] = prompt("Tug`ilgan sana");
+// console.log(menu);
+
+// let name = +prompt("Jasur kim o`zi?");
+// console.log(name + 10);
+//
+// let firstName = "Samar";
+// console.log("Meni ismim " + firstName);
+
+// let name = "baxtishod";
+// console.log(`Meni ismim ${name}`);
+
+// const name = prompt("Sizning ismingiz nima?");
+// console.log(`Uning ismi ${name}`);
+
+// const num = 4 + "radio";
+// console.log(typeof num);
+
+// let name = 1;
+// console.log(++name);
+
+// let x = 1;
+// console.log(--x);
+
+// let name = 4;
+// let age = "4";
+// console.log(name === age);
+
+// const a = true;
+// const b = false;
+// console.log(a || b);
+// console.log(a && b);
+
+// let a = true;
+// let b = false;
+// console.log(a && !b);
+// console.log(!a || b);
+
+// const a = true;
+// const b = false;
+// console.log(a && (b || !a));
